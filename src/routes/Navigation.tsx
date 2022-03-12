027255656d206b1d2f6e5542a90dc9189a0d5a3b
Alejandro Olmos <manuelalejandro.soto1@gmail.com>
2022-03-12 16:48:23 -0300

    lazyload nested routes done

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,59 +1,48 @@
+import { Suspense } from "react"
 import { BrowserRouter, Routes, Route, NavLink, Navigate } from "react-router-dom"
 import logo from '../logo.svg'
+import { routes } from "./Routes"
 
 const Navigation = () => {
   return (
-    <BrowserRouter>
+    <Suspense fallback={ <span>Loading...</span> }>
+        <BrowserRouter>
         <div className="main-layout">
             <nav>
                 <img src={logo} alt="React Logo" />
                 <ul>
-                    <li>
-                        <NavLink 
-                            to='/home' 
-                            className={ ({ isActive }) => isActive ? 'nav-active' : ''} 
-                        >
-                            Home
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink 
-                            to='/about' 
-                            className={ ({ isActive }) => isActive ? 'nav-active' : ''} 
-                        >
-                            About
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink 
-                            to='/users' 
-                            className={ ({ isActive }) => isActive ? 'nav-active' : ''} 
-                        >
-                            Users
-                        </NavLink>
-                    </li>
+                    { 
+                        routes.map(({path, to, name}) => (
+                            <li key={path}>
+                                <NavLink
+                                    to={to}
+                                    className={ ({ isActive }) => isActive ? 'nav-active' : ''}
+                                >
+                                    {name}
+                                </NavLink>
+                            </li>
+                        )) 
+                    }
                 </ul>
             </nav>
             <Routes>
-                <Route 
-                    path="about" 
-                    element={<h1>About Page</h1>} 
-                />
-                <Route 
-                    path="users" 
-                    element={<h1>Users Page</h1>} 
-                />
-                <Route 
-                    path="home" 
-                    element={<h1>Home Page</h1>} 
-                />
+                {
+                    routes.map(({ path, Component}) => 
+                        <Route
+                            key={path}
+                            path={path} 
+                            element={ <Component /> } 
+                        />    
+                    )
+                }
                 <Route 
                     path="/*" 
-                    element={<Navigate to='/home' replace />} 
+                    element={ <Navigate to={routes[0].to} replace />} 
                 />
             </Routes>
         </div>
     </BrowserRouter>
+    </Suspense>
   )
 }
 
